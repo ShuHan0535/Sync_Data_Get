99bcbcec3e5528b0e029dcae703c1c91848e20b3
shu.han <fanshu_h@163.com>
2021-09-03 17:45:01 +0800

    V0.4

--- tools/init.sh
+++ tools/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "start deploy a cross-compilation environment..."
-tar xvf cross_compilation_tool/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -C cross_compilation_tool > /dev/null
+tar xvf gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz  > /dev/null
 echo "deploy a cross-compilation environment end"
 
